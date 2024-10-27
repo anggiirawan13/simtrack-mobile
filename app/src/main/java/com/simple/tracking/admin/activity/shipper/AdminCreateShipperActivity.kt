bb4e3b1355399aedf5d749c2e779023d755e8bc3
anggiirawan13 <irawananggi76@gmail.com>
2024-10-27 12:55:47 +0700

    feat: init

--- app/src/main/java/com/simple/tracking/admin/activity/shipper/AdminCreateShipperActivity.kt
+++ app/src/main/java/com/simple/tracking/admin/activity/shipper/AdminCreateShipperActivity.kt
@@ -2,11 +2,30 @@ package com.simple.tracking.admin.activity.shipper
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.google.android.material.textfield.MaterialAutoCompleteTextView
 import com.simple.tracking.R
 
 class AdminCreateShipperActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin_create_shipper)
+
+        val roles = arrayOf("User 1", "User 2", "User 3")
+
+        val roleSpinner = findViewById<MaterialAutoCompleteTextView>(R.id.roleSpinner)
+        val adapter =
+            ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, roles)
+        roleSpinner.setAdapter(adapter)
+
+        roleSpinner.setOnFocusChangeListener { _, hasFocus ->
+            if (hasFocus) {
+                roleSpinner.showDropDown()
+            }
+        }
+
+        roleSpinner.setOnClickListener {
+            roleSpinner.showDropDown()
+        }
     }
 }
--- app/src/main/java/com/simple/tracking/admin/activity/shipper/AdminUpdateShipperActivity.kt
+++ app/src/main/java/com/simple/tracking/admin/activity/shipper/AdminUpdateShipperActivity.kt
@@ -2,11 +2,30 @@ package com.simple.tracking.admin.activity.shipper
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.google.android.material.textfield.MaterialAutoCompleteTextView
 import com.simple.tracking.R
 
 class AdminUpdateShipperActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin_update_shipper)
+
+        val roles = arrayOf("Admin", "Commissioner", "Director", "Shipper")
+
+        val roleSpinner = findViewById<MaterialAutoCompleteTextView>(R.id.roleSpinner)
+        val adapter =
+            ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, roles)
+        roleSpinner.setAdapter(adapter)
+
+        roleSpinner.setOnFocusChangeListener { _, hasFocus ->
+            if (hasFocus) {
+                roleSpinner.showDropDown()
+            }
+        }
+
+        roleSpinner.setOnClickListener {
+            roleSpinner.showDropDown()
+        }
     }
 }
